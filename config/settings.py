e8dea8393bfc5cb86fbfe6b98d1f5c1a344d220d
liukun <963190750@qq.com>
2023-05-18 13:57:25 +0800

    测试.gitignore

--- config/settings.py
+++
@@ -1,9 +0,0 @@
-
-HOST = '127.0.0.2'
-PORT = 5000
-SECRET_KEY = '123456'
-
-try:
-    from config.dbSettings import *
-except ImportError:
-    pass
--- manage.py
+++
@@ -1,5 +0,0 @@
-from codes import app
-
-
-if __name__ == '__main__':
-    app.run()
